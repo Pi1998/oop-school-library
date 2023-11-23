d64a2c4ef20cb25a81ea1c4960e36e4cfb5b9b0c
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-23 20:52:22 +0630

    Fix linters

--- student.rb
+++ student.rb
@@ -1,15 +1,14 @@
-require "./person.rb"
+require './person'
 
 class Student < Person
-    attr_accessor :classroom
+  attr_accessor :classroom
 
-    def initialize(id, age, classroomm, name: "Unknown", parent_permission: true)
-        super(id, age, name: name, parent_permission: parent_permission)
-        @classroom = classroom
-    end
+  def initialize(id, age, _classroomm, name: 'Unknown', parent_permission: true)
+    super(id, age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        puts "¯\(ツ)/¯"
-    end
-
+  def play_hooky
+    puts '¯(ツ)/¯'
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,15 +1,15 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(id, age, specialization, name: "Unknown", parent_permission: true)
-        super(id, age, name: name, parent_permission: parent_permission)
-        @specialization = specialization
-    end
+  def initialize(id, age, specialization, name: 'Unknown', parent_permission: true)
+    super(id, age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
 
-    # override can_use_services?
-    def can_use_services?
-        true
-    end
+  # override can_use_services?
+  def can_use_services?
+    true
+  end
 end
