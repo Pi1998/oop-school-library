6f8aadf6c31d742da97fcd512b9e4b83e87cf8f6
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-24 02:36:09 +0630

    Update student class

--- student.rb
+++ student.rb
@@ -1,14 +1,10 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
-
-  def initialize(id, age, classroom, name: 'Unknown', parent_permission: true)
-    super(id, age, name: name, parent_permission: parent_permission)
-    @classroom = classroom
-  end
-
-  def play_hooky
-    puts '¯(ツ)/¯'
-  end
+    attr_accessor :classroom
+  
+    def initialize(id, age, classroom, name: "Unknown", parent_permission: true)
+      super(id, name)
+      @classroom = classroom
+    end
 end
