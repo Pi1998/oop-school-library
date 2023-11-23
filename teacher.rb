187d48986b89e7b059ca776b6d8a147de58831ff
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-24 02:36:27 +0630

    Update teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,15 +1,15 @@
 require './person'
 
 class Teacher < Person
-  attr_accessor :specialization
-
-  def initialize(id, age, specialization, name: 'Unknown', parent_permission: true)
-    super(id, age, name: name, parent_permission: parent_permission)
-    @specialization = specialization
-  end
-
-  # override can_use_services?
-  def can_use_services?
-    true
-  end
+    attr_accessor :specialization
+  
+    def initialize(id, age, specialization, name: "Unknown", parent_permission: true)
+      super(id, name)
+      @specialization = specialization
+    end
+  
+    # Override 
+    def can_use_services?
+      true
+    end
 end
