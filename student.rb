47c12bd4ba8e6ee95c780db2e81b8ade5b2ee3e7
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-25 01:04:26 +0630

    Add student to classroom

--- student.rb
+++ student.rb
@@ -1,10 +1,11 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(id, _age, classroom, name: 'Unknown')
+  def initialize(id, age, classroom, name: 'Unknown')
     super(id, name)
     @classroom = classroom
+    classroom&.add_student(self)
   end
 end
