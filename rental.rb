3a8b855f72962fa00cf00b50e93ec46515327979
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-25 21:47:29 +0630

    add getter and setter for book and person

--- rental.rb
+++ rental.rb
@@ -1,12 +1,11 @@
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
+  attr_accessor :date, :book, :person
 
-  def initialize(book, person, date)
+  def initialize(date, book, person)
     @book = book
     @person = person
     @date = date
-    book.add_rental(self) # Add the rental to the book
-    person.add_rental(self) # Add the rental to the person
+    book.add_rental(person, date) 
+    person.add_rental(book, date) 
   end
 end
